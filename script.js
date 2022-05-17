c0c7ff62ccbe0879525d911a0770aa495bf7e95d
Esteban Basili <esteban.ba@gmail.com>
2022-05-17 19:35:47 +0200

    add style, customize art elements, refine DOM Manipulation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // DEFINE START SCREEN
 
 const startBtn = document.getElementById("start-button");
+const centralPanel = document.querySelector(".central-panel");
+const instructions = document.querySelector("#instructions");
+const dashboard = document.querySelector("#dashboard");
 
 function setStartBtn() {
     startBtn.innerText = "Start Game";
@@ -10,29 +13,49 @@ function setStartBtn() {
 
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext("2d");
+const theScore = document.getElementById('score');
+const highScore = document.getElementById('hi-score');
 
+function setStopBtn() {
+    startBtn.innerText = "Stop Game";
+}
 
-function setResetBtn() {
-    startBtn.innerText = "Restart Game";
+function drawScore() {
+    theScore.innerText = score;
+}
+
+function drawHiScore() {
+    console.log(score);
+    if (newHiScore < score) {
+        newHiScore = score;
+        highScore.innerHTML = newHiScore;
+    }
 }
 
 // DEFINE GAME OVER SCREEN
 
-
+function setRestartBtn() {
+    startBtn.innerText = "Restart Game";
+}
 
 // DEFINE GLOBAL PARAMETERS
 
-let gridSize = 20;
+let gridSize = 25;
 
 function randomPosition (min, max) {
     return Math.round((Math.random() * (max-min) + min) / gridSize) * gridSize;
 }
 
 let score = 0;
+let newHiScore = 0;
 
 let gameOver = false;
 
 // DEFINE PLAYER PARAMETERS
+
+// const playerImgArray = {a: "/images/player.png", b: "/images/staff.png"};
+// const playerImg = new Image();
+// playerImg.src = playerImgArray.a;
 
 let playerX = randomPosition(0, canvas.width - gridSize);
 let playerY = randomPosition(0, canvas.height - gridSize);
@@ -41,11 +64,17 @@ const playerH = gridSize;
 let speedX = gridSize;
 let speedY = 0;
 
-let player = [{x: gridSize * 4, y: gridSize * 2}];
+let theHead = {x: playerX, y: playerY};
+let theTailPart = {x: playerX, y: playerY};
+
+let player = [{x: playerX, y: playerY}]
 
 let drawParts = (eachPart) => {
-    ctx.fillStyle = "gray";
+    // ctx.drawImage(playerImg, eachPart.x, eachPart.y, playerW, playerH);
+    ctx.fillStyle = "#90a4ae";
+    ctx.strokeStyle = "black";
     ctx.fillRect(eachPart.x, eachPart.y, playerW, playerH);
+    ctx.strokeRect(eachPart.x, eachPart.y, playerW, playerH);
 }
 
 let drawPlayer = () => {
@@ -69,8 +98,8 @@ let drawPlayer = () => {
 function moveCharacter() {
     const head = {x: player[0].x + speedX, y: player[0].y + speedY};
     player.unshift(head);
-    if (player[0].x === foodX && player[0].y === foodY) {
-        gotFood();
+    if (player[0].x === artX && player[0].y === artY) {
+        gotArt();
     } else {
         player.pop();
     }
@@ -102,32 +131,43 @@ function changeDirection(event) {
 
 // DEFINE FOOD PARAMETERS
 
-let foodX;
-let foodY;
+const artImg = new Image();
 
-function randomFood() {
-    foodX = randomPosition(0, canvas.width - gridSize);
-    foodY = randomPosition(0, canvas.height - gridSize);
+let artArray = ["/images/1.png", "/images/2.png", "/images/3.png", "/images/4.png", "/images/5.png", "/images/6.png", "/images/7.png",];
+
+artImg.src = artArray[parseInt(Math.random() * (artArray.length))];
+
+let randomArt = () => {
+    artImg.src = artArray[parseInt(Math.random() * (artArray.length))];
+};
+
+let artX;
+let artY;
+
+function randomArtPosition() {
+    artX = randomPosition(0, canvas.width - gridSize);
+    artY = randomPosition(0, canvas.height - gridSize);
     player.forEach(element => {
-        const playerPosition = element.x === foodX && element.y === foodY;
+        const playerPosition = element.x === artX && element.y === artY;
         if (playerPosition) {
-            randomFood();
+            randomArtPosition();
         }
     });
 }
 
-randomFood();
+randomArtPosition();
 
-let drawFood = () => {
-    ctx.fillStyle = "orange";
-    ctx.fillRect(foodX, foodY, gridSize, gridSize); 
+let drawArt = () => {
+    ctx.drawImage(artImg, artX, artY, gridSize, gridSize);
+    // ctx.fillStyle = "orange";
+    // ctx.fillRect(artX, artY, gridSize, gridSize); 
 }
 
-function gotFood() {
+function gotArt() {
     score += 1;
-    randomFood();
-    drawFood();
-    console.log("score: " + score);
+    randomArtPosition();
+    randomArt();
+    drawArt();
 }
 
 // DEFINE ANIMATION
@@ -136,11 +176,13 @@ let intervalId;
 
 let animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawFood()
-    moveCharacter()
+    drawArt()
     drawPlayer()
+    moveCharacter()
+    drawScore()
     if (gameOver) {
         cancelAnimationFrame(intervalId);
+        setRestartBtn()
         console.log("Game Over!")
     } else {
         intervalId = setTimeout(() => {
@@ -152,16 +194,35 @@ let animate = () => {
 // DEFINE GAME EVENTS
 
 let startGame = () => {
+    instructions.style.visibility = "hidden";
+    instructions.style.display = "none";
+    canvas.style.visibility = "visible";
+    canvas.style.display = "block";
+    dashboard.style.visibility = "visible";
     animate();
 }
 
+let stopGame = () => {
+    console.log("Game Stopped")
+    gameOver = true
+}
+    
 let restartGame = () => {
+    console.log("Game Restarted")
+    // reset player
     playerX = randomPosition(0, canvas.width - gridSize);
     playerY = randomPosition(0, canvas.height - gridSize);
     speedX = gridSize;
     speedY = 0;
-
-let player = [{x: gridSize * 4, y: gridSize * 2}];
+    player = [{x: gridSize * 4, y: gridSize * 2}];
+    // reset food
+    randomArtPosition();
+    drawArt();
+    // reset global
+    gameOver = false;
+    drawHiScore();
+    score = 0;
+    animate();
 }
 
 window.addEventListener("load", () => {
@@ -170,11 +231,15 @@ window.addEventListener("load", () => {
 startBtn.addEventListener("click", () => {
     if (startBtn.innerText === "Start Game"){
         startGame();
-        setResetBtn();
-    } else {
+        setStopBtn();
+    } else if (startBtn.innerText === "Stop Game") {
+        stopGame();
+        setRestartBtn();
+    } else if (startBtn.innerText === "Restart Game") {
         restartGame();
-        setStartBtn();
+        setStopBtn();
     }
 });
 
 document.addEventListener("keydown", changeDirection)
+
